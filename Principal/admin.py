740511165c38afa560153fb546cc439128cc840e
Jhonatan Laguna <jhonlpdev@gmail.com>
2015-06-18 00:51:52 -0700

    Realizadas migraciones de la base de datos

--- Principal/admin.py
+++ Principal/admin.py
@@ -7,12 +7,12 @@ class ProyectoAdmin(admin.ModelAdmin):
 	list_display = ('id','nombre','categoria',)
 	list_filter = ('categoria',)
 	list_editable = ('nombre',)
-	raw_id_fields = ('categoria')
+	raw_id_fields = ('categoria',)
 
 class ProyectoInline(admin.ModelAdmin):
 	model = Proyecto
 	extra = 1
-	raw_id_fields = ('categoria')
+	
 
 class CategoriaAdmin(admin.ModelAdmin):
 	inlines = [ProyectoInline]
