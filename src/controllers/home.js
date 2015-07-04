563df4aafe74210c4db4942cbed42ccddaec76a1
Jhonatan Laguna <JhonLPdev@gmail.com>
2015-07-03 23:38:16 -0700

    Controladores y vistas de home y portfolio terminados

--- src/controllers/home.js
+++ src/controllers/home.js
@@ -1,9 +1,20 @@
-var _ = require('underscore');
+var _ = require('underscore'),
+	Home = require('../models/home');
 
 var homeController = function (server, users) {
 	console.log("ready");
-	server.get('/', function (req,res) {
-		res.render('index');
+
+	server.get('/', function(req,res) {
+		Home.find({})
+		.exec(function(err, homes){
+			var homesAsJson = _.map(homes, function (home) {
+				return home.toJSON();
+			});
+
+			res.render('index',{
+				homes : homes
+			});
+		});
 	});
 };
 module.exports = homeController;
--- src/controllers/portfolio.js
+++ src/controllers/portfolio.js
@@ -1,9 +1,20 @@
-var _ = require('underscore');
+var _ = require('underscore'),
+	Proyecto = require('../models/proyecto');
 
 var portfolioController = function (server, users) {
 	console.log("ready");
-	server.get('/portfolio', function (req,res) {
-		res.render('portfolio');
+
+	server.get('/portfolio', function(req,res) {
+		Proyecto.find({})
+		.exec(function(err, proyectos){
+			var proyectosAsJson = _.map(proyectos, function (proyecto) {
+				return proyecto.toJSON();
+			});
+
+			res.render('portfolio',{
+				proyectos : proyectos
+			});
+		});
 	});
 };
 module.exports = portfolioController;
