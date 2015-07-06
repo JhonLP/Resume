180258c838d25113e09e88064bb4c8d5d96546d4
Jhonatan Laguna <JhonLPdev@gmail.com>
2015-07-06 00:36:41 -0700

    Vista y controlador de Resume funcionando correctamente, agregado modulo async

--- src/controllers/resume.js
+++ src/controllers/resume.js
@@ -1,28 +1,28 @@
 var _ = require('underscore'),
+	async = require("async"),
 	Resume = require('../models/resume'),
 	Education = require('../models/education');
 
 var resumeController = function (server, users) {
 	console.log("ready");
 
-
 	server.get('/resume', function(req,res) {
-		Resume.find({})
-		.exec(function(err, resumes){
 
-			var resumesAsJson = _.map(resumes, function (resume) {
-				return resume.toJSON();
-			});
-			Education.find({})
-			.exec(function(err, educations){
-				var educationsAsJson = _.map(educations, function (education) {
-					return education.toJSON();
-				});
-			});
-			res.render('resume',{
-				resumes : resumes,
-				educations : educations
-			});
+		var resumenes = Resume.find({});
+		var educacion = Education.find({});
+
+		var resultados = {
+			resumes : resumenes.exec.bind(resumenes),
+			educations : educacion.exec.bind(educacion)
+		};
+
+		async.parallel(resultados, function (error,informacion) {
+			if (error) {
+				res.status(500).send(error);
+				return;
+			}
+			console.log(informacion);
+			res.render('resume',informacion);
 		});
 	});
 };
